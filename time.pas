0c5bdf1ddcd54d1a681210429e66f7f2c51e15da
Nikitin Daniil <05092005daniil@mail.ru>
2023-09-30 07:58:11 +0300

    Completed the second task time.pas

---
+++ time.pas
@@ -0,0 +1,5 @@
+﻿##
+var year := readinteger('Введите год: ');
+if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then
+  print($'{year} - високосный год')
+else print($'{year} - не високосный год')
