9f4ea96737b9e53ab2f5f4724e72f3c1f351cda6
Nikitin Daniil <05092005daniil@mail.ru>
2023-09-30 08:03:50 +0300

    Completed the fird task time.pas, timer.pas

--- time.pas
+++ time.pas
@@ -1,5 +1,5 @@
 ﻿##
 var year := readinteger('Введите год: ');
 if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then
-  print($'{year} - високосный год')
-else print($'{year} - не високосный год')
+  print($'В {year} году 366 дней')
+else print($'В {year} году 365 дней')
